eb5ac9c477ad84e881cd4a0ef9028c74df502536
Josef Kopriva <jkopriva@redhat.com>
2018-07-31 09:42:13 +0200

    sdfsdfsdf Signed-off-by: Josef Kopriva <jkopriva@redhat.com>

--- helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
+++ helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
@@ -25,7 +25,7 @@ package org.jboss.as.quickstarts.helloworld;
 public class HelloService {
 
     String createHelloMessage(String name) {
-        return "Hello sghjfgdfgsfd" + name + "!";
+        return "Hello sghjfgsfsdfsfsdfsdfsddfghghsdfgsfd" + name + "!";
     }
 
 }
